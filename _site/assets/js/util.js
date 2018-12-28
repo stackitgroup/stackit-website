1e43831d61a57b8e781ed73cf0581a4f29004dd2
Madson C <madson@cardoso.org>
2018-12-28 14:35:57 -0800

    Change scroll events to passive.

--- _site/assets/js/util.js
+++ _site/assets/js/util.js
@@ -183,7 +183,7 @@
 					$this.touchPosX = event.originalEvent.touches[0].pageX;
 					$this.touchPosY = event.originalEvent.touches[0].pageY;
 
-				})
+				}, {passive: true})
 
 				$this.on('touchmove', function(event) {
 
@@ -252,7 +252,7 @@
 			// Event: Prevent certain events inside the panel from bubbling.
 				$this.on('click touchend touchstart touchmove', function(event) {
 					event.stopPropagation();
-				});
+				}, {passive: true});
 
 			// Event: Hide panel if a child anchor tag pointing to its ID is clicked.
 				$this.on('click', 'a[href="#' + id + '"]', function(event) {
