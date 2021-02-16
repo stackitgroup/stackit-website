9c9435cce550128cb06d8f362ac5f054afaadd87
Isaias Garcia <igarcia@progammi.com>
2021-02-16 08:40:51 -0600

    Small fix

--- pages/contact.vue
+++ pages/contact.vue
@@ -8,15 +8,27 @@
         :font-size="['3xl', '4xl', '5xl', '6xl']"
         font-weight="600"
         :max-w="['100%', '100%', '100%', '100%', '60%']"
+        data-aos="fade-zoom-in"
+        data-aos-delay="100"
       >
         Let's talk about your project
       </c-heading>
 
-      <c-text font-size="lg" :max-w="['100%', '100%', '100%', '100%', '60%']">
+      <c-text
+        font-size="lg"
+        :max-w="['100%', '100%', '100%', '100%', '60%']"
+        data-aos="fade-zoom-in"
+        data-aos-delay="200"
+      >
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at porttitor sem.  Aliquam erat volutpat. Donec placerat nisl magna, et faucibus arcu condimentum sed.
       </c-text>
 
-      <c-flex justify="space-between" wrap="wrap">
+      <c-flex
+        justify="space-between"
+        wrap="wrap"
+        data-aos="fade-zoom-in"
+        data-aos-delay="300"
+      >
         <c-stack v-if="!isSent" :w="['100%', '100%', '100%', '50%', '60%']" spacing="6" wrap="wrap">
           <c-input v-model="form.name" h="3.75rem" placeholder="Name" is-required size="lg" />
           <c-flex justify="space-between">
@@ -74,6 +86,8 @@
         :disabled="isFormValid"
         :is-loading="isLoading"
         loading-text="Sending"
+        data-aos="fade-zoom-in"
+        data-aos-delay="300"
         @click="sendMessage()"
       >
         SEND MESSAGE
