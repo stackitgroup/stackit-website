7560702e5167aebb411357f345236b9211c12fb7
Isaias Garcia <igarcia@progammi.com>
2021-02-17 12:10:32 -0600

    Updated cloud code

--- cloud/main.js
+++ cloud/main.js
@@ -1,12 +1,24 @@
-const apiKey = '8790a864aea14e6188bfbd022eb3e44e-3d0809fb-884eaee7'
-const domain = 'sandbox9e6a5f3a22084cd2ae931172d115924c.mailgun.org'
+const apiKey = process.env.MAILGUN_KEY
+const domain = process.env.MAILGUN_DOMAIN
+
 const mailgun = require('mailgun-js')({ apiKey, domain })
+const { WebClient } = require('@slack/web-api')
+
+const channel = `#${process.env.SLACK_CHANNEL}`
+const web = new WebClient(process.env.SLACK_TOKEN)
 
 // eslint-disable-next-line no-undef
 Parse.Cloud.afterSave('Contact', (request) => {
   const email = request.object.get('email')
   const name = request.object.get('name')
+  const phone = request.object.get('phone')
+  const message = request.object.get('message')
 
+  sendEmailToClient(email, name)
+  sendMessageToTeam(email, name, phone, message)
+})
+
+function sendEmailToClient(email, name) {
   const data = {
     from: 'Stackit <no-reply@stackitgroup.com>',
     subject: 'Stackit Contact',
@@ -17,5 +29,40 @@ Parse.Cloud.afterSave('Contact', (request) => {
 
   mailgun.messages().send(data)
     .then(response => console.log('email sent', response))
-    .catch(error => console.error(error))
-})
+    .catch(error => console.error('email error', error))
+}
+
+function sendMessageToTeam(email, name, phone, message) {
+  const blocks = [
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `Hi! *${name}* wants to reach out the Stackit Team.`
+      }
+    },
+    {
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: `*${name}*\n${phone}\n${email}\n`
+      }
+    },
+    {
+      type: 'context',
+      elements: [
+        {
+          type: 'plain_text',
+          text: `${message}\n\n`
+        }
+      ]
+    },
+    {
+      type: 'divider'
+    }
+  ]
+
+  web.chat.postMessage({ blocks, channel })
+    .then(response => console.log('slack sent', response))
+    .catch(error => console.error('slack error', error))
+}
