39dd5366a4a1516f52bc98a81942603cfe187f54
Angel Gonzalez <angel.gonzalez@stackitgroup.com>
2025-09-22 18:53:14 -0400

    fix: footer sizing

--- src/lib/components/site-footer.svelte
+++ src/lib/components/site-footer.svelte
@@ -1,135 +1,131 @@
 <script>
-		import StackitLogoSolidWhite from '$lib/assets/stackit_logo_solid_white.png';
+  import StackitLogoSolidWhite from '$lib/assets/stackit_logo_solid_white.png';
 </script>
 
-<footer class="bg-black text-gray-300 py-20 md:py-24">
-	<div class="container mx-auto px-6">
-		<div class="grid grid-cols-1 md:grid-cols-10 gap-y-12 md:gap-x-16">
-			<div class="md:col-span-7">
-				<a href="#" class="inline-block mb-6">
-					<img
-						src={StackitLogoSolidWhite}
-						alt="Stackit Logo"
-						class="h-6 w-auto"
-					/>
-				</a>
-				<div class="text-gray-300 text-md">
-					<p>
-						Stackit is a software development agency. With a specialty in
-						business systems, SaaS, and critical application design, we're
-						the preferred partner for companies that want to build
-						extraordinary software with a team that shares their standards.
-						We provide a true software development partnership, built on a
-						culture of deep collaboration and rigorous engineering
-						discipline. We give you the confidence of a great in-house team,
-						freeing you to innovate at speed.
-					</p>
-					<p class="mt-4 font-semibold text-gray-300">San Diego, CA</p>
-				</div>
-			</div>
+<footer class="bg-black text-gray-300 py-20 md:py-24 min-h-dvh">
+  <div class="container mx-auto px-6">
+    <div class="grid grid-cols-1 md:grid-cols-10 gap-y-12 md:gap-x-16">
+      <div class="md:col-span-7">
+        <a href="#" class="inline-block mb-6">
+          <img
+            src={StackitLogoSolidWhite}
+            alt="Stackit Logo"
+            class="h-6 w-auto"
+          />
+        </a>
+        <div class="text-gray-300 text-md">
+          <p>
+            Stackit is a software development agency. With a specialty in
+            business systems, SaaS, and critical application design, we're the
+            preferred partner for companies that want to build extraordinary
+            software with a team that shares their standards. We provide a true
+            software development partnership, built on a culture of deep
+            collaboration and rigorous engineering discipline. We give you the
+            confidence of a great in-house team, freeing you to innovate at
+            speed.
+          </p>
+          <p class="mt-4 font-semibold text-gray-300">San Diego, CA</p>
+        </div>
+      </div>
 
-			<div class="md:col-span-3">
-				<h2 class="text-2xl font-bold text-white mb-6">Get In Touch</h2>
-				<div class="space-y-4 text-lg text-gray-300">
-					<p class="flex items-center">
-						<svg
-							class="w-5 h-5 mr-3 text-gray-400"
-							fill="none"
-							stroke="currentColor"
-							viewBox="0 0 24 24"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-							></path>
-						</svg>
-						<a
-							href="mailto:hello@stackitgroup.com"
-							class="hover:text-white transition-colors"
-							>hello@stackitgroup.com</a
-						>
-					</p>
-					<p class="flex items-center">
-						<svg
-							class="w-5 h-5 mr-3 text-gray-400"
-							fill="none"
-							stroke="currentColor"
-							viewBox="0 0 24 24"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2"
-								d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-							></path>
-						</svg>
-						<a href="tel:+16199175387" class="hover:cursor-default"
-							>+1 (619) 917-5387</a
-						>
-					</p>
-					<p class="flex items-center">
-						<svg
-							class="w-5 h-5 mr-3 text-gray-400"
-							fill="currentColor"
-							viewBox="0 0 24 24"
-							xmlns="http://www.w3.org/2000/svg"
-						>
-							<path
-								d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"
-							></path>
-						</svg>
-						<a
-							href="https://www.linkedin.com/company/stackit/"
-							target="_blank"
-							rel="noopener noreferrer"
-							class="font-semibold hover:underline text-gray-400"
-							>LinkedIn</a
-						>
-					</p>
-				</div>
-			</div>
+      <div class="md:col-span-3">
+        <h2 class="text-2xl font-bold text-white mb-6">Get In Touch</h2>
+        <div class="space-y-4 text-lg text-gray-300">
+          <p class="flex items-center">
+            <svg
+              class="w-5 h-5 mr-3 text-gray-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+              ></path>
+            </svg>
+            <a
+              href="mailto:hello@stackitgroup.com"
+              class="hover:text-white transition-colors"
+              >hello@stackitgroup.com</a
+            >
+          </p>
+          <p class="flex items-center">
+            <svg
+              class="w-5 h-5 mr-3 text-gray-400"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
+              ></path>
+            </svg>
+            <a href="tel:+16199175387" class="hover:cursor-default"
+              >+1 (619) 917-5387</a
+            >
+          </p>
+          <p class="flex items-center">
+            <svg
+              class="w-5 h-5 mr-3 text-gray-400"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"
+              ></path>
+            </svg>
+            <a
+              href="https://www.linkedin.com/company/stackit/"
+              target="_blank"
+              rel="noopener noreferrer"
+              class="font-semibold hover:underline text-gray-400">LinkedIn</a
+            >
+          </p>
+        </div>
+      </div>
 
-			<div class="md:col-span-7">
-				<h2 class="text-2xl font-bold text-white mb-4">Stay in the loop</h2>
-				<div class="max-w-lg">
-					<form class="relative" id="newsletter-form">
-						<input
-							id="newsletter-email"
-							type="email"
-							placeholder="Enter your email"
-							autocomplete="off"
-							class="w-full px-1 py-4 bg-transparent text-white border-0 border-b-2 border-gray-700 focus:outline-none focus:ring-0 focus:border-[#3F5FDD] pr-28 transition-colors rounded-none"
-							required
-						/>
-						<button
-							id="newsletter-button"
-							type="submit"
-							class="absolute inset-y-0 right-0 flex items-center px-6 text-white font-semibold hover:text-gray-300 transition-colors"
-						>
-							<span>Subscribe</span>
-						</button>
-					</form>
-					<p
-						id="newsletter-success"
-						class="text-gray-400 mt-2 text-sm hidden"
-					>
-						Welcome to Stackit!
-					</p>
-				</div>
-			</div>
+      <div class="md:col-span-7">
+        <h2 class="text-2xl font-bold text-white mb-4">Stay in the loop</h2>
+        <div class="max-w-lg">
+          <form class="relative" id="newsletter-form">
+            <input
+              id="newsletter-email"
+              type="email"
+              placeholder="Enter your email"
+              autocomplete="off"
+              class="w-full px-1 py-4 bg-transparent text-white border-0 border-b-2 border-gray-700 focus:outline-none focus:ring-0 focus:border-[#3F5FDD] pr-28 transition-colors rounded-none"
+              required
+            />
+            <button
+              id="newsletter-button"
+              type="submit"
+              class="absolute inset-y-0 right-0 flex items-center px-6 text-white font-semibold hover:text-gray-300 transition-colors"
+            >
+              <span>Subscribe</span>
+            </button>
+          </form>
+          <p id="newsletter-success" class="text-gray-400 mt-2 text-sm hidden">
+            Welcome to Stackit!
+          </p>
+        </div>
+      </div>
 
-			<div class="md:col-span-7">
-				<div
-					class="flex flex-col w-auto text-4xl font-semibold text-white text-center md:text-left mx-auto md:mx-0"
-				>
-					<span>Success.</span>
-					<span>Coded.</span>
-				</div>
-			</div>
-		</div>
-	</div>
+      <div class="md:col-span-7">
+        <div
+          class="flex flex-col w-auto text-4xl font-semibold text-white text-center md:text-left mx-auto md:mx-0"
+        >
+          <span>Success.</span>
+          <span>Coded.</span>
+        </div>
+      </div>
+    </div>
+  </div>
 </footer>
