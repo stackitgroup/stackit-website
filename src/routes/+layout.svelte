e56ecb935fb86544c4963eff74d907f1a5d6ebe0
Alefrank Martinez <alefrank.martinez@stackitgroup.com>
2025-09-25 18:01:16 +0100

    fix: update site URL to use dynamic origin for better flexibility

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,7 +14,7 @@
 		= 'Stackit | Helping high-growth leaders innovate at speed with the discipline & collaboration of an in-house team'
 	const siteDescription
 		= 'Build extraordinary software with a partner who shares your standards. Stackit provides an external development team with in-house drive and dedication.'
-	const siteUrl = 'https://www.stackitgroup.com' // Using the final production URL
+	const siteUrl = window.location.origin
 	const siteName = 'Stackit'
 	const socialImage = `${siteUrl}/stackit-logo-social-media-b.png`
 	const keywords
